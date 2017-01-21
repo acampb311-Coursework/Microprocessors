38616073dfe9e00b6931a6f3695f05282482f67f
Adam Campbell <acampb311@outlook.com>
2017-01-21 11:37:18 -0600

    Added Documentation

--- Lab2b.asm
+++ Lab2b.asm
@@ -3,10 +3,13 @@
 ;
 ;Description: This is a program intended to be used as a subroutine for reading
 ; a set of IR sensor values to make appropriate robot navigational decisions.
+; Please note that this file uses tabs, and those tabs are 4 spaces.
 ;
 ;Written By: Adam Campbell
 ;
 ;Date Written: 1/19/2017
+;
+;Date Modified: (1/21/17) - (Adam Campbell) - (Added Documentation)
 ;
 ;******************************************************************************;
 
@@ -16,36 +19,35 @@ Threshold   EQU $55 	       ;Wall Closeness
 
 ;Program - Lab2b
             ORG     $3100   	    ;Starting Address
-            LDAA    $3000   	    ;Load the contents of $3000 into register A
-            STAA    $1800   	    ;Store the contents of register A into address $1800
-            LDAA    $3001   	    ;Load the contents of $3001 into register A
-            STAA    $1801   	    ;Store the contents of register A into address $1801
-            LDAA    $3002   	    ;Load the contents of $3002 into register A
-            STAA    $1802   	    ;Store the contents of register A into address $1802
-	        LDAA 	$1800		    ;Loading sensor data
-            CMPA    #Threshold	    ;Comparing sensor data to the threshold value
-IF1	        BLS		ELSE1		    ;Utilizing fall through logic evaluate if sensor data is higher than the threshold
-            LDAA	#$00		    ;
-            LDAB    #$01            ;
-WHILE1      CMPA    #$0F            ;
+            LDAA    $3000   	    ;Load the sensor data into register A
+            STAA    $1800   	    ;Store the sensor data into address $1800
+            LDAA    $3001   	    ;Load the sensor data into register A
+            STAA    $1801   	    ;Store the sensor data into address $1801
+            LDAA    $3002   	    ;Load the sensor data into register A
+            STAA    $1802   	    ;Store the sensor data into address $1802
+	        LDAA 	$1800		    ;A = sensor data
+            CMPA    #Threshold	    ;Compare sensor data to threshold value
+IF1	        BLS		ELSE1		    ;If (A > Threshold)
+            LDAA	#$00		    ;   A = 0
+            LDAB    #$01            ;   B = 1
+WHILE1      CMPA    #$0F            ;   While (A != 15)
             BEQ     END_WHILE1      ;
-            ABA
-            INCB
-            BRA     WHILE1
-            STAA    $1800
-END_WHILE1  STAA	$1800		    ;Saving the equivalant of the summation of 1 through 5
-ELSE1       LDAA	$1801		    ;
-	        CMPA	#Threshold	    ;
-IF2         BLS		ELSE2		    ;Utilizing fall through logic evaluate if sensor data is higher than the threshold
-	        LDAA	#$00		    ;
-	        STAA	$1801		    ;Storing $00 to the sensor location
-ELSE2       LDAA	$1802		    ;
-	        CMPA    #Threshold	    ;
-IF3	        BLS		ELSE3		    ;Utilizing fall through logic evaluate if sensor data is higher than the threshold
-	        LDAA	$1802		    ;
-	        LDAB	#$10		    ;
-	        SBA					    ;Subtracting the contents of register B from register A and storing back in A
-	        STAA	$1802 		    ;Storing 10 less than the original signal to the sensor location
+            ABA                     ;       A = B + A
+            INCB                    ;       B++
+            BRA     WHILE1          ;   PC = While1
+END_WHILE1  STAA	$1800		    ;$1800 = A
+ELSE1       LDAA	$1801		    ;A = sensor data
+	        CMPA	#Threshold	    ;Compare sensor data to threshold value
+IF2         BLS		ELSE2		    ;If (A > Threshold)
+	        LDAA	#$00		    ;   A = 0
+	        STAA	$1801		    ;   $1801 = A
+ELSE2       LDAA	$1802		    ;A = sensor data
+	        CMPA    #Threshold	    ;Compare sensor data to threshold value
+IF3	        BLS		ELSE3		    ;If (A > Threshold)
+	        LDAA	$1802		    ;   A = (Contents Of)$1802
+	        LDAB	#$10		    ;   B = 10
+	        SBA					    ;   A = A - B
+	        STAA	$1802 		    ;$1802 = A
 ELSE3	    SWI					    ;
             END					    ;
 ;End Program - Lab 2b
